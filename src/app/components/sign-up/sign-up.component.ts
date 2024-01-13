3d198d09d10c090265d4e1716b2a644e252af0ae
Chandan123341 <chandankr0699@gmail.com>
2024-01-13 17:52:48 +0530

    UI: changes

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
